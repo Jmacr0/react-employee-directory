96e577a5539dd849ec2830744871ba771f5585d9
Jmacro <jonmacrohon@gmail.com>
2020-03-25 01:06:41 +1100

    Updates

---
+++ precache-manifest.9f51666b33f3bac1458c77e322092120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcfe8192f9606def120223aed5d48a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f8f45f8b6f2386f427",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "14af8519ed66b822482a",
+    "url": "/static/js/2.53e5ad3b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.53e5ad3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f8f45f8b6f2386f427",
+    "url": "/static/js/main.3f6af1c7.chunk.js"
+  },
+  {
+    "revision": "22e5fb1d163a0f8fe06e",
+    "url": "/static/js/runtime-main.6fb05293.js"
+  }
+]);
